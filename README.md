7874ac35f211c71c4a7d168a9feaa39f236bff52
ariandre123 <77471974+ariandre123@users.noreply.github.com>
2021-01-23 04:15:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# uptime buat bot
---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["link project"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
